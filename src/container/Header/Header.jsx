9433b1e3b6ff00da02a6b40fdf430ac328e6f9f7
Dru Baker <75537547+druskiii@users.noreply.github.com>
2023-11-13 15:23:12 -0500

    Title change

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -34,8 +34,8 @@ const Header = () => {
           </div>
 
           <div className='tag-cmp app__flex'>
+            <p className="p-text">Sustainability Technologist</p>
             <p className="p-text">Software Engineer</p>
-            <p className="p-text">Web Developer</p>
             <p className="p-text">Freelancer</p>
           </div>
         </div>
